db70faa20c8e71cc70af437e21ba1179ab25e76d
imekaB1 <113190118+imekaB1@users.noreply.github.com>
2024-07-03 17:34:40 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -2,12 +2,21 @@ const net = require('net');
 const config = require('./config');
 const codec8EHandler = require('./codec8EHandler');
 
+
 const server = net.createServer(socket => {
     console.log('Teltonika device connected.');
 
     socket.on('data', data => {
         console.log('Data received from the device.');
-        const parsedData = codec8EHandler.parseData(Buffer.from(data));
+        const parsedData = codec8EHandler.parseData(parsedData.from(data));
+        // Create a buffer
+const parsedData = parsedData.from('This is a sample buffer');
+
+// Convert buffer to string
+const string = parsedData.toString('utf-8');
+
+console.log(string);
+
         console.log('Parsed Data:', parsedData);
     });
 
